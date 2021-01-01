a7d29426f406eac033aad0b9c569fe32eef103bd
Faith Magras <bdanielle092@gmail.com>
2021-01-01 17:34:59 -0600

    working on adding clouds

---
+++ JackTheGaint/Assets/Assets+/Assets/Scripts/Background Scripts/BCScaler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BCScaler : MonoBehaviour {
+    
+	void Start () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+
+        float width = sr.sprite.bounds.size.x;
+
+        //background height
+        float worldHeight = Camera.main.orthographicSize * 2.0f;
+        //geting width by  dividing worldHeight from screen height time screen width
+        float worldWidth = worldHeight / Screen.height * Screen.width;
+
+        
+        tempScale.x = worldWidth/ width;
+      
+        transform.localScale = tempScale;
+	}
+	
+	
+}
